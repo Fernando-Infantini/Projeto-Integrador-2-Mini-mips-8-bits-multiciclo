e704c1a60ed10585866c55ee9f5819fb07da1b60
usrr <ronaldygelos.aluno@unipampa.edu.br>
2025-04-30 17:52:59 -0300

    full test

--- tests/test_flags.c
+++ tests/test_flags.c
@@ -5,7 +5,20 @@
 int main(void){
 	for(int i=0;i<11;i++){
 		control_signal* a = uc(i,3);
-		printf("%i %i\n", i, a->RegDst);
+		printf("%i | %i %i %i %i %i %i %i %i %i %i %i %i\n", i,
+			a->pcWrite;
+			a->louD;
+			a->MemWrite;
+			a->irWrite;
+			a->Mem2Reg;
+			a->RegWrite;
+			a->AluSrcA;
+			a->AluSrcB;
+			a->AluFunct;
+			a->pcSrc;
+			a->RegDst;
+			a->branch;
+		);
 		free(a);
 	}
 	return 0;
