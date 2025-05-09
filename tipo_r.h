f56acb6110d6cd56b438f61a287f0c435005894e
usrr <ronaldygelos.aluno@unipampa.edu.br>
2025-05-09 14:08:10 -0300

    FIXING LACK OF MERGE OF AN OLD COMMIT

--- tipo_r.h
+++
@@ -1,6 +0,0 @@
-#include"ula.h"
-
-void execucaoR(mips_instance* block, control_signal* csignal);
-void terminoR(int rd, mips_instance* block, data *mem);
-void busca_inst(int pc, mips_instance* block);
-void decode(mips_instance* block, int pc);
