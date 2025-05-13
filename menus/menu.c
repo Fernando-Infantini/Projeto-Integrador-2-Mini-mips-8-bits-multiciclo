30c8435e6f0aa30d32ebc263871f8052d8a2e425
Enzo32MIPS <enzomoraes.aluno@unipampa.edu.br>
2025-05-13 10:11:29 -0300

    Added menu (still WIP)

--- menus/menu.c
+++ menus/menu.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include "mips_instance.h"
+#include <stdlib.h>
+#include "headersmips_instance.h"
 #include "memoria.h"
 #include "nameing.h"
 #include "stack.h"
@@ -7,7 +8,7 @@
 int main(int argc, char** argv){
 
 	char fileN[64]; //nome do arquivo
-	char tmp;
+	char tmp, opt;
 
 	mips_instance mips={0};
 	state* state_stack = NULL;
@@ -18,8 +19,17 @@ int main(int argc, char** argv){
 	mips.mem[3].inst = (15<<12)+(0<<9)+(1<<6)+1;
 	mips.mem[4].inst = (2<<12)+1;
 
-	do{
+	printf("1)step\n2)show data memory\n3)show registers\n4)show all instructions\n5)make .asm\n6)load data memory data\n7)store data memory data\n8)run\n9)load instruction memory\nb)back\n0)quit\n:");
+	setbuf(stdin,NULL);
+	scanf("%c",opt);
 
+	while (opt != '0'){ 
+
+	case '0':
+	exit(0);
+	break;
+
+	case '1':
 		for(int i=0;i<8;i++){
 			printf("|%i",mips.reg[i]);
 		}
@@ -37,12 +47,52 @@ int main(int argc, char** argv){
 		instruction_name_finder((mips.RI>>12),(mips.RI&7),I);
 		microinstruction_name_finder(mips.microinstruction,M);
 		printf("pc:%i|%s %s\n",mips.pc,I,M);
-		printf("continue? ");
-		exec(&mips);
+	break;
 
-		do tmp = getc(stdin); while(tmp=='\n');
+	case '2':
+	for(int i=0;i<16;i++){
+		for(int j=0;j<16;j++){
+			printf("|%i %i",mips.mem[16*i+j].data[1], mips.mem[16*i+j].data[0]);
+		}
+		printf("|\n");
+	}
+	printf("\n");
+	break;
 
-	}while(tmp=='y');
+	case '3':
+	for(int i=0;i<8;i++){
+		printf("|%i",mips.reg[i]);
+	}
+	printf("|\n");
+	break;
 
+	case '4':
+	printf("Work in progress...\n");
+	break;
+	}
+
+	case '5':
+	printf("Work in progress...\n");
+	break;
+
+	case '6':
+	printf("Work in progress...\n");
+	break;
+
+	case '7':
+	printf("Work in progress...\n");
+	break;
+
+	case '8':
+	printf("Work in progress...\n");
+	break;
+
+	case '9':
+	printf("Work in progress...\n");
+	break; 
+
+	case 'b':
+	printf("Work in progress...\n");
+	break; 
 	return 0;
 }
