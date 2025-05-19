65e73f176a35a8755c912df3a2a000a8e32d0d92
usrr <ronaldygelos.aluno@unipampa.edu.br>
2025-05-19 15:48:02 -0300

    fix separation of concerns

--- headers/memoria.h
+++ headers/memoria.h
@@ -1,7 +1,7 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 #include "mips_instance.h"
-void ler_mem(mips_instance* state);
+int ler_mem(mips_instance* state, const char* name);
 int binario_para_decimal(char binario[], int inicio, int fim, int complemento2);
 void decod(data* a);
 int gen_asm(mips_instance* state, const char* name);
