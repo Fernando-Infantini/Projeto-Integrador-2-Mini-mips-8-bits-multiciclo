1a6d28b2483d43ddc00281296ed55341e09716eb
Enzo32MIPS <enzomoraes.aluno@unipampa.edu.br>
2025-05-22 17:12:19 -0300

    fixed naming of case 9 in menu

--- menus/menu.c
+++ menus/menu.c
@@ -11,7 +11,7 @@
 void print_memory(mips_instance* mips);
 void print_registers(mips_instance* mips);
 void const_size(char* out, int in);
-void print_uc_sig(mips_instance* mips);
+void print_multi(mips_instance* mips);
 
 int main(int argc, char** argv){
 
@@ -32,7 +32,7 @@ int main(int argc, char** argv){
 
     while (opt != '0'){
 
-	printf("\n1)step\n2)show data memory\n3)show registers\n4)show all instructions\n5)make .asm\n6)store data\n7)run\n8)load instruction memory\n9)show control unit signals\nb)back\n0)quit\n:");
+	printf("\n1)step\n2)show data memory\n3)show registers\n4)show all instructions\n5)make .asm\n6)store data\n7)run\n8)load instruction memory\n9)show multicycle only units\nb)back\n0)quit\n:");
 
 	do scanf("%c",&opt); while (opt == '\n');
 
@@ -97,7 +97,7 @@ int main(int argc, char** argv){
 	break;
 
 	case '9':
-		print_uc_sig(&mips);
+		print_multi(&mips);
 	break;
 
 	case 'b':
@@ -141,8 +141,8 @@ void print_registers(mips_instance* mips){
 	return;
 }
 
-void print_uc_sig(mips_instance* mips){
-	printf("\n|PC: %i|RI: %i |A: %i|B: %i|RDM: %i|AluOut: %i|\n", mips->pc, mips->RI, mips->A, mips->B, mips->RDM, mips->aluOut);
+void print_multi(mips_instance* mips){
+	printf("\n|PC: %i|RI: %i|RDM: %i|A: %i|B: %i|AluOut: %i|\n", mips->pc, mips->RI, mips->RDM,mips->A, mips->B,  mips->aluOut);
 	return;
 }
 
